79398a5b5c2d84e5df896553a9342de4327a8950
Blake Benthall <blake@benthall.net>
2013-03-04 17:45:24 -0800

    quick cleanup

--- index.php
+++ index.php
@@ -11,26 +11,9 @@
 <link rel="stylesheet" href="css/screen.css" media="screen, projection">
 <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="http://www.rockymountainruby.com/favicon.ico" type="image/ico" />
-<script type="text/javascript" src="js/prefixfree.js"></script>
-<script type="text/javascript" src="js/modernizr-2.0.6.js"></script>
 <meta name = "viewport" content = "initial-scale=1.0, width=device-width" />
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-38990344-1']);
-  _gaq.push(['_setDomainName', 'ghostpost.io']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-</head>
-
-<style type="text/css">
+<style>
 
     .avatarz {
         width: 58px;
@@ -91,6 +74,21 @@
 
 </style>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38990344-1']);
+  _gaq.push(['_setDomainName', 'ghostpost.io']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
 <body>
     <nav>
         <a href="index.html"><img src = "images/logo.png"></a>
