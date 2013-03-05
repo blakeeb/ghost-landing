ab332d93199951752f344a33f37c7c25fe285e94
Blake Benthall <blake@benthall.net>
2013-03-04 17:09:40 -0800

    Added Google Analytics

--- index.php
+++ index.php
@@ -14,7 +14,20 @@
 <script type="text/javascript" src="js/prefixfree.js"></script>
 <script type="text/javascript" src="js/modernizr-2.0.6.js"></script>
 <meta name = "viewport" content = "initial-scale=1.0, width=device-width" />
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38990344-1']);
+  _gaq.push(['_setDomainName', 'ghostpost.io']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 
 <style type="text/css">
