d9dad99eb0a4abace4f4a8e60bdacf2d12214447
Blake Benthall <blake@benthall.net>
2013-03-04 16:58:45 -0800

    moved to index.php

--- index.php
+++ index.php
@@ -1,1 +1,126 @@
-<p>I'm just here for the <code>.htaccess</code></p>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="description" content="Anonymous Event-Based Mobile Chat. ">
+<meta name="keywords" content="chat, anonymous">
+<meta charset="UTF - 8">
+<meta name="copyright Ghost Post" content="2013">
+
+<title>Ghost Post</title>
+
+<link rel="stylesheet" href="css/screen.css" media="screen, projection">
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="http://www.rockymountainruby.com/favicon.ico" type="image/ico" />
+<script type="text/javascript" src="js/prefixfree.js"></script>
+<script type="text/javascript" src="js/modernizr-2.0.6.js"></script>
+<meta name = "viewport" content = "initial-scale=1.0, width=device-width" />
+
+</head>
+
+<style type="text/css">
+
+    .avatarz {
+        width: 58px;
+        height: auto;
+    }
+
+    .buttonz {
+        width: 150px;
+        position: relative;
+        display: inline-block;
+        margin-left: 5%;
+        font-size: 12pt;
+        -webkit-text-fill-color: #1e1e1e;
+        border: 0px;
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+        padding: 10px 10px 10px 10px;
+        text-shadow: -1px -1px 0 rgba(0,0,0,0.3);
+        font-weight:bold;
+        text-align: center;
+        background-color: #66ffff;
+        background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #66ffff), color-stop(100%, #6699ff));
+        background-image: -webkit-linear-gradient(top, #66ffff, #6699ff);
+        background-image: -moz-linear-gradient(top, #66ffff, #6699ff);
+        background-image: -ms-linear-gradient(top, #66ffff, #6699ff);
+        background-image: -o-linear-gradient(top, #66ffff, #6699ff);
+        background-image: linear-gradient(top, #66ffff, #6699ff);filter:progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr=#66ffff, endColorstr=#6699ff);
+
+    }
+
+    .emailblock {
+        width: 150px;
+        position: relative;
+        display: inline-block;
+    }
+
+    .create input {
+        width: 96%;
+        padding: 0 2%;
+        border-radius: 0.5em;
+        border: 0;
+        height: 40px;
+        background: transparent;
+        border: 1px dashed #60F7FC;
+        font-size: 1.8em;
+        color: #f2f2f2;
+        margin-top: 5%;
+    }
+
+    .create input:focus {
+    outline:none;
+    }
+
+    .twitter {
+        margin-top: 2%;
+    }
+
+</style>
+
+<body>
+    <nav>
+        <a href="index.html"><img src = "images/logo.png"></a>
+    </nav>
+    <div class = "main">
+        <div class = "avatar cf">
+            <img src = "images/avatars/av1.png" class="avatarz">
+            <img src = "images/avatars/av2.png" class="avatarz">
+            <img src = "images/avatars/av3.png" class="avatarz">
+            <img src = "images/avatars/av4.png" class="avatarz">
+            <img src = "images/avatars/av5.png" class="avatarz">
+            <img src = "images/avatars/av6.png" class="avatarz">
+            <img src = "images/avatars/av7.png" class="avatarz">
+            <img src = "images/avatars/av8.png" class="avatarz">
+            <h1>Sign up for the future of supernatural chat.</h1>
+
+            <!-- Begin MailChimp Signup Form -->
+            <div id="mc_embed_signup">
+                <form action="http://baratunde.us2.list-manage1.com/subscribe/post?u=5fe2c35965f2ba5253995ead9&amp;id=1b8a97c255" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+                <div class="mc-field-group create" style="display: inline-block" >
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Your email">
+                </div>
+                    <div id="mce-responses" style="display: inline-block">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>
+                    <div class="clear" style="display: inline-block"><input type="submit" value="SUBMIT!" name="subscribe" id="mc-embedded-subscribe" class="button buttonz">
+                    </div>
+                </form>
+            </div>
+
+            <!--End mc_embed_signup-->
+
+            <div class="twitter">
+                <a class="twitter-timeline" href="https://twitter.com/GhostPostApp" data-widget-id="308705372322013185">Tweets by @GhostPostApp</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
+
+
+
+
+        </div>
+    </div>
+</body>
+</html>
